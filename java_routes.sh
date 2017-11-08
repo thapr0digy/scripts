cf61a2aab366215ca2f11200b2db36ea4de9ede8
thapr0digy <lindomusic@gmail.com>
2017-11-08 18:25:31 -0500

    Delete java_routes.sh

--- java_routes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#eth=$(ip addr | egrep -v "\.0\.1/24" | grep -B2 "inet 10\\."| grep UP | egrep -o "eth.")
-ppp=tun0
-route add default gw $(ifconfig $ppp | grep 'inet 10' | awk '{print $2}') 
-#sed -i.backup -e 's/search.*$/search us.proofpoint.com app.proofpoint.com corp.proofpoint.com spt.proofpoint.com proofpoint.com ppops.net/' /etc/resolv.conf
-cp /etc/resolv.conf.proofpoint /etc/resolv.conf
